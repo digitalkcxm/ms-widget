2ca316575ed48b882cbeb43b12fd88d0ae60bc43
lucasluzdev <lucasluzdev@tutanota.com>
2020-09-18 12:24:21 -0300

    health endpoint

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
   app.use('/api/v1/company', require('../routes/company'))
   app.use('/api/v1/widget', require('../routes/widget'))
+  app.use('/api/v1/health', require('../routes/health'))
 }
